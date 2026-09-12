01416bf65b9f5d1fa955e3b40643f4d9346bd217
zhouzhengjie4-sketch <zhouzhengjie4@gmail.com>
2026-09-13 04:47:04 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
 'use strict';
 // 修改页面或图标后，递增版本号。
-const CACHE='suikouji-shell-v1';
+const CACHE='suikouji-shell-2026.09.13.6';
 const FILES=['./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));
 });
 self.addEventListener('activate',event=>{
  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('suikouji-shell-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('message',event=>{
+ if(event.data?.type==='SKIP_WAITING')self.skipWaiting();
 });
 self.addEventListener('fetch',event=>{
  const req=event.request;
